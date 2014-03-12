0ed3aa6c9188f23e3b177e5b0b7a6717d62ead42
Nick Schonning <nschonni@gmail.com>
2014-03-12 12:41:02 -0400

    Support running from behind a proxy

--- build/index.js
+++ build/index.js
@@ -6,10 +6,12 @@ var fs       = require('fs');
 // enable GitHub Flavored Markdown dialect
 require('./gfm-dialect.js');
 require('./nav-dialect.js');
-var url      = "https://raw.github.com/bower/bower/master/README.md";
 var template = Hogan.compile(fs.readFileSync(path.join(__dirname,'./template.{'), 'utf-8'));
 
-request(url, function (error, response, body) {
+request({
+    url: "https://raw.github.com/bower/bower/master/README.md",
+    proxy: process.env.http_proxy
+  } , function (error, response, body) {
   body = body.replace(/ \[\!\[Build Status\].+$/m, '');
   //assuming that links not containing http are relative to bower main repo
   body = body.replace(/\[(.*)\]\((?!http)(.*)\)/g, '\[\$1\](https://github.com/bower/bower/blob/master/\$2\)');
