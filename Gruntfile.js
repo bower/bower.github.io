7af13785cb5570858c800a94ccd81ec8a06973a6
Julian Motz <me@julianmotz.com>
2016-11-09 18:17:28 +0100

    Implemented dist file minification
    1. When using grunt-contrib-concat and UglifyJS directly after, not all license comments will be preserved
    2. When using UglifyJS a "use strict"; statement will be added to the document. Unfortunately d3.js doesn't work in strict mode since it accesses `this.document` and `this` doesn't reference the global object
    3. Therefore I'm using UglifyJS to concat all files and to compress them and replace "use strict"; statements at the end
    4. To preserve license comments uglify-save-license is used
    
    Also updated dependencies
    
    Closes #272

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,67 @@
-var request = require('request');
-
-module.exports = function( grunt ) {
+const saveLicense = require('uglify-save-license');
+module.exports = grunt => {
 
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    concat: {
-      css: {
-        src: [
-          'node_modules/normalize.css/normalize.css',
-          'css/plottable.css',
-          'css/base.css',
-          'css/masthead.css',
-          'css/modules.css',
-          'css/blog-post.css',
-          'css/medias.css'
-        ],
-        dest: 'dist/index.css'
-      },
-      js: {
-        src: [
-          'node_modules/d3/d3.js',
-          'node_modules/es6-promise/dist/es6-promise.js',
-          'node_modules/whatwg-fetch/fetch.js',
-          'node_modules/mustache/mustache.js',
-          'node_modules/lodash/lodash.js',
-          'node_modules/mark.js/dist/mark.js',
-          'js/plottable.js',
-          'js/index.js'
-        ],
-        dest: 'dist/index.js'
+    cssmin: {
+      dist: {
+        files: {
+          'dist/index.css': [
+            'node_modules/normalize.css/normalize.css',
+            'css/plottable.css',
+            'css/base.css',
+            'css/masthead.css',
+            'css/modules.css',
+            'css/blog-post.css',
+            'css/medias.css'
+          ]
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        options: {
+          preserveComments: saveLicense,
+          screwIE8: true
+        },
+        files: {
+          'dist/index.js': [
+            'node_modules/d3/d3.min.js',
+            'node_modules/es6-promise/dist/es6-promise.min.js',
+            'node_modules/whatwg-fetch/fetch.js',
+            'node_modules/mustache/mustache.min.js',
+            'node_modules/mark.js/dist/mark.min.js',
+            'node_modules/lodash/lodash.min.js',
+            'js/plottable.js',
+            'js/index.js'
+          ]
+        }
+      }
+    },
+    replace: {
+      dist: {
+        options: {
+          patterns: [{
+            // it's necessary to remove "use strict"; as d3.js otherwise throws
+            // an error. It uses `this.document` and `this` doesn't
+            // reference to the global object in strict mode
+            match: /"use strict";/g,
+            replacement: ''
+          }]
+        },
+        files: [{
+          src: ['dist/index.js'],
+          dest: 'dist/index.js'
+        }]
       }
     }
   });
 
-  grunt.registerTask( 'default', [
-    'concat'
+  grunt.registerTask('default', [
+    'uglify',
+    'cssmin',
+    'replace'
   ]);
 
 };
