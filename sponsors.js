2c86f8e9617240a4decf189088acb14db3648ee3
Adam Stankiewicz <sheerun@sher.pl>
2020-04-09 11:01:22 +0200

    Update

--- sponsors.js
+++ sponsors.js
@@ -59,6 +59,11 @@ const exceptions = ['digital-bank-guide', 'alex-owner']
 
 const datasup = [
   {
+    name: 'trivaltech',
+    href: 'https://trivaltech.com/',
+    text: 'trivaltech.com'
+  },
+  {
     name: 'bellwether-capital',
     href: 'https://www.bellwethercap.io/',
     text: 'bellwethercap.io'
